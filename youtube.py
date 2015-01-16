571cee918b352e6c4ee07b8f900451aded94842d
Jacob Minshall <minshallj@fingertiptech.com>
2015-01-15 19:11:14 -0800

    search for multiple words without quoting them

--- youtube.py
+++ youtube.py
@@ -15,7 +15,7 @@ class YoutubeSearch:
     """A new tab of youtube video search page will open up, displaying the videos matching the specified arguments inputted"""
     def search(self):
         import webbrowser
-        url = "https://www.youtube.com/results?search_query=" + self.toProperSearchQuery()
+        url = "https://www.youtube.com/results?search_query=" + '+'.join(self.to_search)
     
         """Check if args.filter array has items and if a sort_by argument was given"""
         if self.filter:
@@ -24,19 +24,13 @@ class YoutubeSearch:
             url += "&search_sort=" + self.sort_by
     
         webbrowser.open(url)
- 
-    """
-    Change Spaces to Plus Signs
-    Ex) "This Is My Search Text" -> "This+Is+My+Search+Text"
-    """
-    def toProperSearchQuery(self):
-        words = self.to_search.split()
-        return "+".join(words)
+
 
 def initArgParse():
     """Create All Options"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("search", action="store", help="Search Query")
+    parser.add_argument("search", action="store", nargs='+',
+                        help="Search Query")
     parser.add_argument("-u", "--upload-date", action="append",
                       help="Upload Date", dest="filter")
     parser.add_argument("-t", "--type", action="append", help="Video Type",
